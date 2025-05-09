a6f42ba9d8203e26f308f465ba4c439ff818f3ee
Meital Lerner <meitalr555@gmail.com>
2025-05-09 14:44:02 +0300

    UPDATE

--- src/main/java/com/ashcollege/CorsConfig.java
+++ src/main/java/com/ashcollege/CorsConfig.java
@@ -1,7 +1,8 @@
 package com.ashcollege;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
@@ -13,7 +14,7 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://localhost:8081",   // אם את על 8081
                         "http://localhost:19006",  // Expo‑Go
                         "https://math-journey-front.onrender.com"
-                )
+                ).allowedMethods("GET","POST","PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(false);      // לא עובדים עם קוקיז – אפשר false
 
